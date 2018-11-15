47f2b56b1a939d24d480ef981908fff93f2c938b
strivema <marui@yinker.com>
2018-11-15 10:43:02 +0800

    学习spring

---
+++ src/main/java/com/xiaowei/spring5/HomeTest.java
@@ -0,0 +1,14 @@
+package com.xiaowei.spring5;
+
+/**
+ * @athour Marie
+ * @date 2018/11/15 10:32 AM
+ **/
+public class HomeTest {
+    public static void main(String[] args) {
+        HomeController homeController = new HomeController();
+        homeController.home();
+
+    }
+
+}
